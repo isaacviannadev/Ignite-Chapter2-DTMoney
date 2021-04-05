5feeb516c7f6ceddd87774c89425e4cc33ce4012
Isaac <isaacsvianna@gmail.com>
2021-04-05 11:17:49 -0300

    Add: components core

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className='App'>
-      <h1>Teste</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
